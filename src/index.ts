066452f127d912b89bf943e5876b0c2c28e2d106
Vikash Kesarwani <vikash.ckiiita30@gmail.com>
2022-10-02 15:01:13 +0000

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import BooleanFake from "./categories/boolean";
-import FakeDataProvider from "./provider";
+import BooleanFake from "./categories/boolean/fake";
+import FakeDataProvider, { IFakeDataProvider } from "./provider";
 
+export class FakeData {
+  readonly boolean: BooleanFake
+
+  constructor(provider: IFakeDataProvider) {
+    this.boolean = new BooleanFake(provider)
+  }
+}
 
 const provider = new FakeDataProvider
 
-const boolean = new BooleanFake(provider)
-
-export class FakeData {
-  readonly boolean = boolean
-}
-
-export default new FakeData
+export default new FakeData(provider)
