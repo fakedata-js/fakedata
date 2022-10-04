3069c9953f0a667ca0bff78a55c06b549aeb1392
Vikash Chandra Kesarwani <vikchand@adobe.com>
2022-10-04 20:32:59 +0530

    Updates new coverage threshol

--- jest.config.js
+++ jest.config.js
@@ -43,7 +43,7 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 80,
+      branches: 100,
       functions: 100,
       lines: 100,
       statements: 100
