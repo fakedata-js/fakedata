8ff38487bf8a9cf2dc1a301cb5bf10ba2c8a93d4
Vikash Kesarwani <vikash.ckiiita30@gmail.com>
2022-10-09 11:06:05 +0000

    fixup

--- src/categories/array.ts
+++ src/categories/array.ts
@@ -1,24 +1,21 @@
-import { normalizeConfig as _normalizeConfig } from "../fake"
-
-interface GeneratorFn<T> { (): T }
-export interface ArrayFake<T> {
+type GeneratorFn<T> = () => T
+export interface ArrayFakeConfig<T> {
   length: number
   fn: GeneratorFn<T>
 }
 
-const normalizeConfig = <T>(config: ArrayFake<T>): ArrayFake<T> => {
-  if (config.length == null || typeof config.length != 'number') {
+const normalizeConfig = <T>(config: ArrayFakeConfig<T>): ArrayFakeConfig<T> => {
+  if (config.length == null || typeof config.length !== 'number') {
     throw new Error('Array length must be a number')
   }
-  if (config.fn == null || typeof config.fn != 'function') {
+  if (config.fn == null || typeof config.fn !== 'function') {
     throw new Error('Generator must be a function')
   }
-  const fConfig = _normalizeConfig({} as ArrayFake<T>, config)
 
-  return fConfig
+  return config
 }
 
-export default function ArrayFake<T>(length: number, fn: GeneratorFn<T> ): T[] {
+export default function ArrayFake<T> (length: number, fn: GeneratorFn<T>): T[] {
   const fConfig = normalizeConfig({ length, fn })
   return Array(fConfig.length).fill(undefined).map(() => fConfig.fn())
 }
