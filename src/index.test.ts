fc22815c67eb1c0d7816fe408485dcab3e57084d
Vikash Kesarwani <vikash.ckiiita30@gmail.com>
2022-10-09 10:12:32 +0000

    fix: Adds shortcut for number and string fakers

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,4 +4,16 @@ describe('Entry point for fake data', () => {
   it('Generates a fake boolean value', () => {
     expect(`${fake.bool()}`).toMatch(/true|false/)
   })
+
+  it('Generates a fake integer', () => {
+    expect(`${fake.int()}`).toMatch(/\d+/)
+  })
+
+  it('Generates a fake floating point number', () => {
+    expect(`${fake.number()}`).toMatch(/\d+\.\d+/)
+  })
+
+  it('Generates a fake string', () => {
+    expect(`${fake.string()}`).toMatch(/[a-zA-Z0-9]+/)
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import BooleanFake from './categories/boolean'
 import IntegerFake from './categories/integer'
+import NumberFake from './categories/number'
+import StringFake from './categories/string'
 
 export class FakeData {
   readonly bool = BooleanFake
   readonly int = IntegerFake
+  readonly number = NumberFake
+  readonly string = StringFake
 }
 
 export default new FakeData()
