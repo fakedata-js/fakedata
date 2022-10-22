c41eb2e79bec1f622bf65c53ae7a760c1bab4ba8
Vikash Kesarwani <vikash.ckiiita30@gmail.com>
2022-10-22 17:27:43 +0000

    fix: build error

--- src/categories/date.ts
+++ src/categories/date.ts
@@ -1,7 +1,11 @@
 import BasePlugin from '../core/base'
 
 export default class DatePlugin extends BasePlugin {
-  today (): Date {
+  any (): Date {
     return new Date()
+  }
+
+  today (): Date {
+    return this.any()
   }
 }
